7612bfb5caa9f654635f42927b5ab570eaf87887
Michiko  Inoue <inouem888@gmail.com>
2024-10-28 11:54:59 +0000

    Add function of post

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,5 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
-    
+    if request.method == "POST":
+            return HttpResponse("You must have POSTed something")
+    else: 
+        return HttpResponse("request.method")
+
+
+def about_me(request):
+    return HttpResponse("This would be the about page")
