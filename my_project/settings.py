43414a0a85c5313f959c41ff1d45140af5cc3754
Michiko  Inoue <inouem888@gmail.com>
2024-10-28 14:57:06 +0000

    Change debug to false

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=+av9e!=uu#f4u3kcah^86wtnxhw=)b$v(dcry-)00ju+(auaz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-micmic210-djangoproject-i05ej9yy4ov.ws-eu116.gitpod.io' ,'.herokuapp.com']
 
